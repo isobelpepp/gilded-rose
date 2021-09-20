d28a95a7ffa55e20931f1b03503b08b46e623c59
Isobel <isobelpeppiatt@yahoo.co.uk>
2021-09-20 11:30:52 +0100

    Tests for regualr products passing

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 - Each item has a name, SellIn, quality
   - At the end of each day SellIn decreases by 1 (unless it is 0)
   - Quality:
-    - Quality decreases by 1 if SellIn > 0
-    - Quality decreases by 2 if SellIn <= 0
+    - Quality decreases by 1 if SellIn => 0
+    - Quality decreases by 2 if SellIn < 0
     - Quality can not be below 0 or above 50
 
   - Exceptions
@@ -14,4 +14,3 @@
       - by 3 when there are 5 days or less
       - quality drops to 0 after the concert
     - “Conjured” items degrade in Quality twice as fast as normal items
-  
--- spec/gildedRoseSpec.js
+++ spec/gildedRoseSpec.js
@@ -15,5 +15,17 @@ describe("Gilded Rose", function() {
       expect(items[0].name).toEqual("foo");
       expect(items[0].quality).toEqual(9)
     });
+    it("should decrease the sellIn by 1 at the end of the day given a regular item", function() {
+      expect(items[0].name).toEqual("foo");
+      expect(items[0].sellIn).toEqual(2)
+    });
+    it("should decrease quality by 2 if sellIn is less than 0", function() {
+      gildedRose.updateQuality()
+      gildedRose.updateQuality()
+      let updatedItems = gildedRose.updateQuality()
+      expect(updatedItems[0].sellIn).toEqual(-1);
+      expect(updatedItems[0].quality).toEqual(5)
+    });
+
   });
 });
